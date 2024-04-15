3f1fc862aece163993b4c48ec02cfa72b2552d3f
agordey <toni.gordei@gmail.com>
2024-04-15 20:17:50 +0300

    Add java code

---
+++ Driver.java
@@ -0,0 +1,10 @@
+public class Driver {
+
+  private String fio;
+  private int age;
+  private int birthdateYear;
+  private int birthdateMonth;
+  private int birthdateDay;
+  private boolean isMale;
+  private String position;
+}
